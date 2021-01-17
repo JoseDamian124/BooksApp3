f2d3dd6f38af1667fbe9bac3d20f1598e27f2afd
Jose FRancisco Fernandez Damian <19470195@itcampeche.edu.mx>
2021-01-17 17:43:35 -0600

    Version inicial del proyecto

---
+++ BooksApp.Data/BooksContext.cs
@@ -0,0 +1,21 @@
+﻿using BooksApp.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace BooksApp.Data
+{
+    public class BooksContext : DbContext
+    {
+        private const string connectionString =
+                                              "Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = BooksAppData";
+
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Publisher> Publishers { get; set; }
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char lectura;
+            
+            do
+            {
+                Console.WriteLine("[A]gregar | [M]odificar autor | [E]liminar autor | [V]er autor | [S]alir ");
+                Console.Write("Selecciona una opcion: ");
+                lectura = Char.ToUpper(Console.ReadKey().KeyChar);
+                Console.WriteLine();
+                switch (lectura)
+                {
+                    case 'A':
+                        AddAuthor();
+                        break;
+
+                    case 'M':
+                        ModifyAuthor();
+                        break;
+
+                    case 'E':
+                        DeleteAuthor();
+                        break;
+
+                    case 'V':
+                        ShowAuthor();
+                        break;
+
+                    case 'S':
+                        Console.WriteLine("Adios. Programa finalizado");
+                        break;
+                    default:
+                        break;
+                }
+                Console.WriteLine();
+            } while (lectura != 'S');
+            Console.WriteLine("");
+        }
+
+        private static void ShowAuthor()
+        {
+            Console.WriteLine("Mostrando autores...");
+        }
+
+        private static void DeleteAuthor()
+        {
+            Console.WriteLine("Eliminando autor...");
+        }
+
+        private static void ModifyAuthor()
+        {
+            Console.WriteLine("Modificando autor...");
+        }
+
+        private static void AddAuthor()
+        {
+            Console.WriteLine("Agregando un autor...");
+        }
+    }
+}
