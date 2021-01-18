bd72434ca920a13d4043dbb3d376856e74b1d0d8
Jose FRancisco Fernandez Damian <19470195@itcampeche.edu.mx>
2021-01-17 19:22:32 -0600

    Agregar las opciones modificar autores y eliminar autores

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -61,14 +61,50 @@ namespace ConsoleApp
 
         private static void DeleteAuthor()
         {
-            Console.WriteLine("Eliminando autor...");
+            Console.Write("Proporcione el identificador del autor a eliminar: ");
+            int authorId = Convert.ToInt32(Console.ReadLine());
+            var author = context.Authors.FirstOrDefault(a => a.Id == authorId);
+            if (author != null)
+            {
+                Console.WriteLine("Datos actuales del autor:");
+                Console.WriteLine($"Identificador: {author.Id}");
+                Console.WriteLine($"Nombres: {author.FirstName}");
+                Console.WriteLine($"Apellidos: {author.LastName}");
+                context.Authors.Remove(author);
+                context.SaveChanges();
+            }
+            else
+            {
+                Console.WriteLine("Lo siento. El autor a eliminar no existe.");
+            }
         }
 
         private static void ModifyAuthor()
         {
-            Console.WriteLine("Modificando autor...");
+            Console.Write("Proporcione el identificador del autor a modificar: ");
+            int authorId = Convert.ToInt32(Console.ReadLine());
+            var author = context.Authors.FirstOrDefault(a => a.Id == authorId);
+            if (author != null)
+            {
+                Console.WriteLine("Datos actuales del autor:");
+                Console.WriteLine($"Identificador: {author.Id}");
+                Console.WriteLine($"Nombres: {author.FirstName}");
+                Console.WriteLine($"Apellidos: {author.LastName}");
+                Console.WriteLine("Nuevos datos.");
+                Console.Write("Proporcione el nuevo nombre del autor: ");
+                string firstName = Console.ReadLine();
+                Console.Write("Proporcione los apellidos del autor: ");
+                string lastName = Console.ReadLine();
+                author.FirstName = firstName;
+                author.LastName = lastName;
+                context.Authors.Update(author);
+                context.SaveChanges();
+            }
+            else
+            {
+                Console.WriteLine("Lo siento. El autor no existe.");
+            }
         }
-
         private static void AddAuthor()
         {
             Console.WriteLine("Agregando un autor.");
